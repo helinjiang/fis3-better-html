63cb5f73d5be42be0addf42b75703eb84ba15ea3
helinjiang <me@helinjiang.com>
2016-05-27 14:45:23 +0800

    require.async

--- src/pages/mod/main.js
+++ src/pages/mod/main.js
@@ -9,11 +9,13 @@ module.exports = {
         this.addEvent(btn, function () {
             // self.testRequire();
             self.testRequireAsync();
+            self.testLoadJs();
         });
     },
     testRequire: function () {
         /**
          * 这种方式引用，则alert不是异步加载，是同步加载的
+         * 如果代码中这么写，即使没调用，也会将其一起打包，如果的确没用到，则将该require注释掉
          */
         // var Alert = require('/components/alert');
         // Alert.show('alert.js同步加载成功');
@@ -22,12 +24,25 @@ module.exports = {
         /**
          * 这种方式引用，则alert才会异步加载，区别在于传递的是数组
          */
-        require(['/components/alert'], function (Alert) {
+        require.async(['/components/alert'], function (Alert) {
             console.log('---', Alert);
             Alert.show('alert.js异步加载成功');
         });
+
+        // 等效方式1
+        // require.async('/components/alert', function (Alert) {
+        //     console.log('---', Alert);
+        //     Alert.show('alert.js异步加载成功');
+        // });
+
+        // 等效方式2，可以从mod.js中的源码中看得到
+        // require(['/components/alert'], function (Alert) {
+        //     console.log('---', Alert);
+        //     Alert.show('alert.js异步加载成功');
+        // });
     },
     testLoadJs: function () {
+        // 异步加载脚本文件，不做任何回调
         require.loadJs('http://mat1.gtimg.com/www/asset/lib/jquery/jquery/jquery-1.11.1.min.js');
     },
     addEvent: function (dom, handler) {
