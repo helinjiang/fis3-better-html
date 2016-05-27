3ce1dabb34258445df3244ee954d12c1cc13104f
helinjiang <me@helinjiang.com>
2016-05-27 11:09:30 +0800

    使用mod.js异步加载和同步加载

---
+++ src/components/alert/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by linjianghe on 2016/5/27.
+ */
+
+exports.show = function (msg) {
+    alert(msg || '什么信息都没有');
+};
--- src/pages/mod/main.js
+++ src/pages/mod/main.js
@@ -5,8 +5,19 @@ require('/components/1-0/main.js');
 
 var btn = document.getElementById('btn');
 var handler = function() {
-    alert('handler');
-    require(['/components/2-0/main.js']);
+    /**
+     * 这种方式引用，则alert不是异步加载，是同步加载的
+     */
+    // var Alert = require('/components/alert');
+    // Alert.show('alert.js同步加载成功');
+
+    /**
+     * 这种方式引用，则alert才会异步加载，区别在于传递的是数组
+     */
+    require(['/components/alert'], function(Alert){
+        console.log('---', Alert);
+        Alert.show('alert.js异步加载成功');
+    });
 };
 
 if (btn.addEventListener) {
